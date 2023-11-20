dc9cb618e2379ca415ae3b9966a6b58284f8ad4a
Juan Sosa <juan.msosa22@gmail.com>
2023-11-20 00:32:00 -0300

    Añado implementacion de clase Senior

--- src/main/java/edu/fiuba/algo3/Gladiador/senority/Senority.java
+++ src/main/java/edu/fiuba/algo3/Gladiador/senority/Senority.java
@@ -1,4 +1,4 @@
-package edu.fiuba.algo3.Gladiador.Senority;
+package edu.fiuba.algo3.Gladiador.senority;
 
 import edu.fiuba.algo3.Gladiador.Energia;
 
--- src/main/test/edu/fiuba/algo3/entrega_1/CasoDeUso9.java
+++ src/main/test/edu/fiuba/algo3/entrega_1/CasoDeUso9.java
@@ -1,13 +1,5 @@
 package edu.fiuba.algo3.entrega_1;
 
-import edu.fiuba.algo3.Casillero;
-import edu.fiuba.algo3.Gladiador.Energia;
-import edu.fiuba.algo3.Equipamientos.Casco;
-import edu.fiuba.algo3.Equipamientos.Equipamiento;
-import edu.fiuba.algo3.Gladiador.Gladiador;
-import edu.fiuba.algo3.Gladiador.Senority.Novato;
-import edu.fiuba.algo3.Gladiador.Senority.Senority;
-import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
