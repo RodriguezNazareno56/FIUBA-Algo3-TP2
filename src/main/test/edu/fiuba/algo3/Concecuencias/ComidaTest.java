0e7ef3936edaf99e413a259c4583490c97a52951
Favio <fcondori@fi.uba.ar>
2023-11-20 09:49:13 -0300

    agregue el test para Comida

---
+++ src/main/test/edu/fiuba/algo3/Concecuencias/ComidaTest.java
@@ -0,0 +1,30 @@
+package edu.fiuba.algo3.Concecuencias;
+
+
+import edu.fiuba.algo3.Equipamientos.*;
+import edu.fiuba.algo3.Gladiador.Energia;
+import edu.fiuba.algo3.Gladiador.Gladiador;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.mockito.ArgumentMatchers;
+import org.mockito.Mockito;
+
+import static org.junit.jupiter.api.Assertions.*;
+import static org.mockito.Mockito.times;
+
+
+class ComidaTest {
+    @Test
+    public void afectarGladiadorHaceQueGladiadorIncrementeSuEquipamiento() {
+        // Arrange
+        Gladiador gladiador = Mockito.mock(Gladiador.class);
+        Comida comida= new Comida();
+
+        // Act
+        comida.afectarGladiador(gladiador);
+
+        // Assert
+        Mockito.verify(gladiador, Mockito.times(1))
+                .comer(Mockito.any(Energia.class));
+    }
+}
