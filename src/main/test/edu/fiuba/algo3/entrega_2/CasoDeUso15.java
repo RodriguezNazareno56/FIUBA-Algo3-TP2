5a3fecd26fa018047274d87866dbfd5e36bd3408
Nazareno <nazarodriguez56@gmail.com>
2023-11-22 16:16:01 -0300

    Agrego las fimas de los test de casos de uso de entrega 2

---
+++ src/main/test/edu/fiuba/algo3/entrega_2/CasoDeUso15.java
@@ -0,0 +1,10 @@
+package edu.fiuba.algo3.entrega_2;
+
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+public class CasoDeUso15 {
+
+    @Test
+    public void verificarLecturaYConversionUnidadesModeloDominioJsonEnemigos(){}
+}
