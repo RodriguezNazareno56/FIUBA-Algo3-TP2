bf098c1ada4e844100af201e86f422caffff45f5
Nazareno <nazarodriguez56@gmail.com>
2023-11-22 19:52:38 -0300

    Quito el getEquipamiento y el getEnergia, lo reemplazo por verificar comportamiento. Si posee el equipo correcto (sin equipo) al ser atacado por una fiera recibe un daño de 20 puntos de energia, si el gladiador fue inicializado con la energia correcta (20 puntos), tras el ataque debe quedar incapacitado para moverce

--- src/main/test/edu/fiuba/algo3/entrega_1/CasoDeUso1.java
+++ src/main/test/edu/fiuba/algo3/entrega_1/CasoDeUso1.java
@@ -1,12 +1,15 @@
 package edu.fiuba.algo3.entrega_1;
 
+import edu.fiuba.algo3.Concecuencias.FieraSalvaje;
 import edu.fiuba.algo3.Equipamientos.Equipamiento;
 import edu.fiuba.algo3.Equipamientos.SinEquipamiento;
 import edu.fiuba.algo3.Gladiador.Energia;
 import edu.fiuba.algo3.Gladiador.Gladiador;
 import edu.fiuba.algo3.Gladiador.senority.Novato;
 import edu.fiuba.algo3.Gladiador.senority.Senority;
+import edu.fiuba.algo3.MovimientoExeption;
 import edu.fiuba.algo3.casilleros.Casillero;
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
@@ -16,8 +19,7 @@ public class CasoDeUso1 {
      * Por enunciado: Cada gladiador comienza con 20 puntos de energía, sin equipamiento.
      */
     @Test
-    public void verificarQueElJugadorEmpiezaConLaEnergiaYEquipamientoCorrespondiente() {
-        // TODO: refactorizar para quitar los getEnergia() y getEquipamiento()
+    public void verificarQueElJugadorEmpiezaConLaEnergiaYEquipamientoCorrespondiente() throws Exception {
         // Arrange
         Energia energia = new Energia(20);
         Equipamiento equipamiento = new SinEquipamiento();
@@ -26,8 +28,12 @@ public class CasoDeUso1 {
         Gladiador gladiador = new Gladiador(energia, equipamiento, casillero, senority);
 
         // Assert
-        assertEquals(gladiador.obtenerEquipamiento(), equipamiento);
-        assertEquals(gladiador.getEnergia(), 20);
-
+        // Un gladiador debe iniciar sin equipamiento y con 20 puntos de energia.
+        // Un gladiador sin equipamiento al ser atacado por una fiera salvaje recibe 20 puntos de energia
+        // Si la inicializacion fue correcta, entonces tras el ataque debe quedar sin energia
+        // y por ende incapaz de moverse
+        gladiador.recibirConsecuencia(new FieraSalvaje());
+        Throwable exception= Assertions.assertThrows(MovimientoExeption.class, gladiador::avanzar);
+        assertEquals("El gladiador no se puede mover sin energia", exception.getMessage());
     }
 }
