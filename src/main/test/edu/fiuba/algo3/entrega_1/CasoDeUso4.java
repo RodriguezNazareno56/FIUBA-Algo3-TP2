3e6cedc26fbb00e5b7c9bf673763429bd10ebee4
Nazareno <nazarodriguez56@gmail.com>
2023-11-23 00:33:32 -0300

    Retorno a utilizar el gladiador.getEnergia() para casos de uso 4, 5 y 6

--- src/main/test/edu/fiuba/algo3/entrega_1/CasoDeUso4.java
+++ src/main/test/edu/fiuba/algo3/entrega_1/CasoDeUso4.java
@@ -5,6 +5,7 @@ import edu.fiuba.algo3.Concecuencias.Consecuencia;
 import edu.fiuba.algo3.Concecuencias.FieraSalvaje;
 import edu.fiuba.algo3.Equipamientos.Equipamiento;
 import edu.fiuba.algo3.Equipamientos.Casco;
+import edu.fiuba.algo3.Equipamientos.SinEquipamiento;
 import edu.fiuba.algo3.Gladiador.Energia;
 import edu.fiuba.algo3.Gladiador.Gladiador;
 import edu.fiuba.algo3.Gladiador.senority.Novato;
@@ -22,40 +23,25 @@ public class CasoDeUso4 {
 
     private Gladiador gladiador;
 
-    private final int cantidadDeCasilleros = 4;
-
     @BeforeEach
     void setUp() {
-        CasillerosFactory casillerosFactory = new CasillerosFactory();
-        ICasillero casillero = casillerosFactory.construirCasilleros(cantidadDeCasilleros);
-        Energia energia = new Energia(1);
-        Equipamiento equipamiento = new Casco();
+        Casillero casillero = new Casillero(0);
+        Energia energia = new Energia(20);
+        Equipamiento equipamiento = new SinEquipamiento();
         Senority senority = new Novato();
         this.gladiador = new Gladiador(energia, equipamiento, casillero, senority);
     }
 
     @Test
     public void verificarQueSiRecibeComidaIncrementaEnergiaEn15() throws Exception {
-
         // Arrange
         Comida comida = new Comida();
-        Consecuencia fieraSalvaje = new FieraSalvaje();
 
         // Act
         this.gladiador.recibirConsecuencia(comida);
 
-
-        this.gladiador.recibirConsecuencia(fieraSalvaje);
-        //por la energia debido a la comida el gladiador con casco deberia poder avanzar
-        this.gladiador.avanzar(1);
-
-        //no deberia poder avanzar tras recibir una segunda fiera
-        this.gladiador.recibirConsecuencia(fieraSalvaje);
-
         // Assert
-        Throwable exception= Assertions.assertThrows(MovimientoException.class, () -> {
-            gladiador.avanzar(1);
-        });
-        assertEquals("El gladiador no se puede mover sin energia", exception.getMessage());
+        assertEquals(gladiador.getEnergia(), 35);
     }
 }
+
