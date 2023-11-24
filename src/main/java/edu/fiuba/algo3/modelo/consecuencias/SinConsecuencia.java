33e98cadb39aca410157fba03ffe965b45b4eeaa
RodriguezNazareno56 <43357298+RodriguezNazareno56@users.noreply.github.com>
2023-11-24 20:34:47 -0300

    Consecuencia SinConcecuencia renombrada a SinConsecuencia, typo (#91)

--- src/main/java/edu/fiuba/algo3/modelo/consecuencias/SinConsecuencia.java
+++ src/main/java/edu/fiuba/algo3/modelo/consecuencias/SinConsecuencia.java
@@ -3,7 +3,7 @@ package edu.fiuba.algo3.modelo.consecuencias;
 import edu.fiuba.algo3.modelo.gladiador.Gladiador;
 import edu.fiuba.algo3.modelo.gladiador.TriunfoException;
 
-public class SinConcecuencia implements Consecuencia {
+public class SinConsecuencia implements Consecuencia {
     @Override
     public void afectarGladiador(Gladiador gladiador) throws TriunfoException {}
 }
