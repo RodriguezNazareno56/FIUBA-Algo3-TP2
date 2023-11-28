29aba01b5bf489143eda0c50c849ca58259f90af
Nazareno <nazarodriguez56@gmail.com>
2023-11-28 04:46:51 -0300

    añado las firmas de los test de la entrega 3

---
+++ src/main/test/edu/fiuba/algo3/entrega_3/CasoDeUso19Test.java
@@ -0,0 +1,11 @@
+package edu.fiuba.algo3.entrega_3;
+
+import org.junit.jupiter.api.Test;
+
+public class CasoDeUso19Test {
+
+    @Test
+    public void simularVerificarQueElJugadorGanaUnaPartida() {
+        //TODO: falta implementar
+    }
+}
---
+++ src/main/test/edu/fiuba/algo3/entrega_3/CasoDeUso20Test.java
@@ -0,0 +1,11 @@
+package edu.fiuba.algo3.entrega_3;
+
+import org.junit.jupiter.api.Test;
+
+public class CasoDeUso20Test {
+
+    @Test
+    public void simularVerificarQueElJugadorPierdeUnaPartida() {
+        // TODO: falta implementar
+    }
+}
