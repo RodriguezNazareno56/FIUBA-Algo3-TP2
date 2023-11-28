637e28ade0cd7533fb9119f44873b20cab70aae1
Nazareno <nazarodriguez56@gmail.com>
2023-11-27 23:11:02 -0300

    Hago publico los test de caminoTest

--- src/main/test/edu/fiuba/algo3/modelo/camino/CaminoTest.java
+++ src/main/test/edu/fiuba/algo3/modelo/camino/CaminoTest.java
@@ -11,7 +11,7 @@ import java.util.List;
 
 import static org.junit.jupiter.api.Assertions.*;
 
-class CaminoTest {
+public class CaminoTest {
 
     private Camino camino;
     private ICelda celdaSalida;
@@ -19,7 +19,7 @@ class CaminoTest {
     private ICelda celdaLlegada;
 
     @BeforeEach
-    void setUp() {
+    public void setUp() {
         List<ICelda> celdas = new ArrayList<>();
         this.celdaSalida = Mockito.mock(Celda.class);
         celdas.add(celdaSalida);
@@ -32,40 +32,40 @@ class CaminoTest {
     }
 
     @Test
-    void getCasilleroSalida() {
+    public void getCasilleroSalida() {
         assertEquals(this.celdaSalida, this.camino.getCasilleroSalida());
     }
 
     @Test
-    void proximoEn1Posicion() {
+    public void proximoEn1Posicion() {
         // En un camino con 3 celdas, si avanzo 1 posiciones desde la celdaSalida termino en la celdaCamino
         ICelda celdaResultado = this.camino.proximoEnNPosiciones(this.celdaSalida, 1);
         assertEquals(this.celdaCamino, celdaResultado);
     }
 
     @Test
-    void proximoEn2Posiciones() {
+    public void proximoEn2Posiciones() {
         // En un camino con 3 celdas, si avanzo 2 posiciones desde la celdaSalida termino en la celdaLlegada
         ICelda celdaResultado = this.camino.proximoEnNPosiciones(this.celdaSalida, 2);
         assertEquals(this.celdaLlegada, celdaResultado);
     }
 
     @Test
-    void anteriorEn1Posicion() {
+    public void anteriorEn1Posicion() {
         // En un camino con 3 celdas, el anterior en 1 posicion desde la celdaLlegada es la celdaCamino
         ICelda celdaResultado = this.camino.anteriorEnNPosiciones(this.celdaLlegada, 1);
         assertEquals(this.celdaCamino, celdaResultado);
     }
 
     @Test
-    void anteriorEn2Posiciones() {
+    public void anteriorEn2Posiciones() {
         // En un camino con 3 celdas, el anterior en 2 posiciones desde la celdaLlegada es la celdaSalida
         ICelda celdaResultado = this.camino.anteriorEnNPosiciones(this.celdaLlegada, 2);
         assertEquals(this.celdaSalida, celdaResultado);
     }
 
     @Test
-    void getMitadDeCamino() {
+    public void getMitadDeCamino() {
         // La mitad del camino es la CeldaCamino
         ICelda celdaResultado = this.camino.getMitadDeCamino();
         assertEquals(this.celdaCamino, celdaResultado);
