6bd49ac0accb202eaa41976e6317781eb677bcc5
Nazareno <nazarodriguez56@gmail.com>
2023-11-27 21:21:25 -0300

    Refactor clase camino

--- src/main/java/edu/fiuba/algo3/modelo/camino/Camino.java
+++ src/main/java/edu/fiuba/algo3/modelo/camino/Camino.java
@@ -11,45 +11,32 @@ public class Camino {
         this.celdas = celdas;
     }
 
-    public List<ICelda> getCeldas() {
-        return celdas;
-    }
-
     public ICelda getCasilleroSalida() {
         return this.celdas.get(0);
     }
 
     public ICelda proximoEnNPosiciones(ICelda celdaActual, int posicionesCantidad) {
-        // Encuentra la posición de la celda dada en la lista
-        int posicionCelda = celdas.indexOf(celdaActual);
-
         // Calcula la posición de la celda resultante
-        int posicionResultado = posicionCelda + posicionesCantidad;
+        int posicionResultado = celdas.indexOf(celdaActual) + posicionesCantidad;
 
         // Verifica si la posición calculada está dentro de los límites de la lista
-//        if (posicionResultado >= 0 && posicionResultado < celdas.size()) {
         if (posicionResultado >= 0 && posicionResultado < celdas.size()) {
-                // Devuelve la celda en la posición calculada
-                return celdas.get(posicionResultado);
-            } else {
-                // Si la posición calculada está fuera de la lista, devuelve el último elemento
-                return celdas.get(celdas.size() - 1);
-            }
+            return celdas.get(posicionResultado);
+        } else {
+            // Si la posicion calculada execede la lista de celdas entonces retorno la ultima celda
+            return celdas.get(celdas.size() - 1);
         }
+    }
 
     public ICelda anteriorEnNPosiciones(ICelda celdaActual, int posicionesCantidad) {
-        // Encuentra la posición de la celda dada en la lista
-        int posicionCelda = celdas.indexOf(celdaActual);
-
         // Calcula la posición de la celda resultante
-        int posicionResultado = posicionCelda - posicionesCantidad;
+        int posicionResultado = celdas.indexOf(celdaActual) - posicionesCantidad;
 
         // Verifica si la posición calculada está dentro de los límites de la lista
         if (posicionResultado >= 0) {
-            // Devuelve la celda en la posición calculada
             return celdas.get(posicionResultado);
         } else {
-            // Si la posición calculada está fuera de la lista, devuelve el primer elemento
+            // Si la posicion calculada es menor a 0 entonces retorno la primer celda
             return celdas.get(0);
         }
     }
