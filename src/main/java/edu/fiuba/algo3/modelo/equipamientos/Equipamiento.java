bf97a59d47d11cc604b93d5656d790a8c2ace746
marcos <maservin@fi.uba.ar>
2023-11-30 18:14:32 -0300

    se modifican los tests de EquipamientoIncrementado

--- src/main/java/edu/fiuba/algo3/modelo/equipamientos/Equipamiento.java
+++ src/main/java/edu/fiuba/algo3/modelo/equipamientos/Equipamiento.java
@@ -2,11 +2,4 @@ package edu.fiuba.algo3.modelo.equipamientos;
 
 public abstract class Equipamiento implements Afectable {
     public abstract Equipamiento incrementar();
-
-    @Override
-    public boolean equals(Object o) {
-        if (this == o) return true;
-        if (o == null || getClass() != o.getClass()) return false;
-        return true;
-    }
 }
