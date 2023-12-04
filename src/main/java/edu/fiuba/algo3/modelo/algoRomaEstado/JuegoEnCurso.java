d4fc6f78133903f93572f64a7377a295bca0c821
Favio <fcondori@fi.uba.ar>
2023-12-04 16:11:06 -0300

    refactor: metodo jugarTurno en JuegoEnCurso

--- src/main/java/edu/fiuba/algo3/modelo/algoRomaEstado/JuegoEnCurso.java
+++ src/main/java/edu/fiuba/algo3/modelo/algoRomaEstado/JuegoEnCurso.java
@@ -2,10 +2,8 @@ package edu.fiuba.algo3.modelo.algoRomaEstado;
 
 import edu.fiuba.algo3.modelo.AlgoRoma;
 import edu.fiuba.algo3.modelo.JuegoEnCursoException;
-import edu.fiuba.algo3.modelo.constantes.AlgoRomaConstantes;
 import edu.fiuba.algo3.modelo.gladiador.Gladiador;
 
-import static edu.fiuba.algo3.modelo.constantes.AlgoRomaConstantes.SE_PUEDE_AGREGAR_GLADIADOR_EN_JUEGO_EN_CURSO;
 public class JuegoEnCurso extends EstadoJuego{
 
 
@@ -15,12 +13,8 @@ public class JuegoEnCurso extends EstadoJuego{
 
         @Override
         public void agregarGladiador(String nombreGladiador) throws JuegoEnCursoException {
-                if( SE_PUEDE_AGREGAR_GLADIADOR_EN_JUEGO_EN_CURSO ){
-                        algoRoma.agregarGladiadorALaLista(nombreGladiador);
-                }
-                else{
-                        throw new JuegoEnCursoException("No se pueden agregar gladiadores en un juego en curso");
-                }
+                throw new JuegoEnCursoException("No se pueden agregar gladiadores en un juego en curso");
+
         }
         @Override
         public void jugarTurno() throws Exception {
@@ -29,12 +23,10 @@ public class JuegoEnCurso extends EstadoJuego{
                         JuegoTerminado juegoTerminado = new JuegoTerminado(algoRoma);
                         algoRoma.setEstadoJuego( juegoTerminado );
                         juegoTerminado.jugarTurno();
-                        return;
                 }
-
-                algoRoma.jugarTurnoSegunEstado(this);
-
-
+                else{
+                        algoRoma.jugarTurnoSegunEstado(this);
+                }
         }
 
         @Override
