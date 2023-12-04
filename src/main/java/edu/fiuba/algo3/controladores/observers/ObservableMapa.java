ffaec24e449c00ea8fe8639759bd717e37a772bf
Nazareno <nazarodriguez56@gmail.com>
2023-12-04 20:28:03 -0300

    movimiento para avanzar el gladiador

---
+++ src/main/java/edu/fiuba/algo3/controladores/observers/ObservableMapa.java
@@ -0,0 +1,27 @@
+package edu.fiuba.algo3.controladores.observers;
+
+import edu.fiuba.algo3.modelo.celda.Coordenada;
+import edu.fiuba.algo3.modelo.gladiador.Gladiador;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class ObservableMapa {
+
+    private List<ObservadorMapa> observadores;
+
+    public ObservableMapa() {
+        this.observadores = new ArrayList<>();
+    }
+
+    // TODO: a discutir si paso la cordenada o la celda
+    public void notificarPosicionamientoDeGladiador(Gladiador gladiador, Coordenada coordenada) {
+        for (ObservadorMapa observador : observadores) {
+            observador.actualizar(gladiador, coordenada);
+        }
+    }
+
+    public void agregarObservador(ObservadorMapa observador) {
+        this.observadores.add(observador);
+    }
+}
--- src/main/java/edu/fiuba/algo3/vista/mapa/components/MapaVista.java
+++ src/main/java/edu/fiuba/algo3/vista/mapa/components/MapaVista.java
@@ -2,7 +2,10 @@ package edu.fiuba.algo3.vista.mapa.components;
 
 import edu.fiuba.algo3.controladores.observers.Observable;
 import edu.fiuba.algo3.controladores.observers.Observador;
+import edu.fiuba.algo3.controladores.observers.ObservadorMapa;
 import edu.fiuba.algo3.modelo.AlgoRoma;
+import edu.fiuba.algo3.modelo.celda.Coordenada;
+import edu.fiuba.algo3.modelo.celda.ICelda;
 import edu.fiuba.algo3.modelo.gladiador.Gladiador;
 import edu.fiuba.algo3.vista.gladiador.GladiadorAnimado;
 import javafx.geometry.Pos;
@@ -11,8 +14,11 @@ import javafx.scene.image.Image;
 import javafx.scene.layout.*;
 
 import java.util.ArrayList;
+import java.util.HashMap;
 
-public class MapaVista extends GridPane implements Observador {
+public class MapaVista extends GridPane implements ObservadorMapa {
+
+    private HashMap<Gladiador, Coordenada> posicionDeGladiadores;
 
     public MapaVista(AlgoRoma algoRoma){
 
@@ -33,15 +39,20 @@ public class MapaVista extends GridPane implements Observador {
                 .getCeldas()
                 .forEach(celda -> this.add(new CeldaVista(50, 50 ), celda.getCoordenada().getX(), celda.getCoordenada().getY()));
 
-        
+        algoRoma.getMapa().agregarObservador(this);
     }
 
-
-
     @Override
-    public void actualizar() {
+    public void actualizar(Gladiador gladiador, Coordenada coordenada) {
+//        if (posicionDeGladiadores.containsKey(gladiador)) {
+//            this.getChildren().remove(coordenada.)
+//        }
+        GladiadorAnimado gladiadorAnimado = new GladiadorAnimado();
+        gladiadorAnimado.setFitWidth(50);
+        gladiadorAnimado.setFitHeight(50);
+        System.out.println("Actualizacion");
+        this.add(gladiadorAnimado, coordenada.getX(), coordenada.getY() );
 
-
-
+//        posicionDeGladiadores.put(gladiador, coordenada);
     }
 }
