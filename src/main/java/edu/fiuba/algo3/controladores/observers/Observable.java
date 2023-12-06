3a776d36eb313f36e5636562e9f5bff07591b3ad
Nazareno <nazarodriguez56@gmail.com>
2023-12-06 18:38:55 -0300

    Hago de la interface Observable una clase abstracta

--- src/main/java/edu/fiuba/algo3/controladores/observers/Observable.java
+++ src/main/java/edu/fiuba/algo3/controladores/observers/Observable.java
@@ -1,6 +1,22 @@
 package edu.fiuba.algo3.controladores.observers;
 
-public interface Observable {
-    void agregarObservador(Observador observador);
-    void notificarAObservadores();
+import java.util.ArrayList;
+
+public abstract class Observable {
+    private final ArrayList<Observador> observadores;
+
+    protected Observable() {
+        this.observadores = new ArrayList<>();
+    }
+
+    public void agregarObservador(Observador observador) {
+        this.observadores.add(observador);
+    }
+
+    // TODO: este metodo debe ser privado
+    public void notificarAObservadores() {
+        for (Observador observador : observadores) {
+            observador.actualizar();
+        }
+    }
 }
