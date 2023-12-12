476f28073a6da342a6bccc8f20cb17d5ed2aef51
Juan Sosa <juan.msosa22@gmail.com>
2023-12-12 17:24:17 -0300

    Agrego imagen de fondo a energia, en caso de ser la energia negativa se muestra en color rojo caso contario en color neutro

--- src/main/java/edu/fiuba/algo3/vista/gladiador/EnergiaVista.java
+++ src/main/java/edu/fiuba/algo3/vista/gladiador/EnergiaVista.java
@@ -4,35 +4,32 @@ import edu.fiuba.algo3.controladores.observers.ObservadorEnergia;
 import edu.fiuba.algo3.modelo.gladiador.Energia;
 import javafx.geometry.Pos;
 import javafx.scene.control.Label;
+import javafx.scene.image.ImageView;
+import javafx.scene.layout.StackPane;
 import javafx.scene.layout.VBox;
 import javafx.scene.paint.Color;
 import javafx.scene.text.Font;
 import javafx.scene.text.FontWeight;
 
-public class EnergiaVista extends VBox implements ObservadorEnergia {
+public class EnergiaVista extends StackPane implements ObservadorEnergia {
 
     private Label valorEnergiaLabel;
 
-    // TODO: Capas otra fuente quede mejor, no logro sacar la separacion vertical entre energia y el valor
     public EnergiaVista(Energia energia, int valorInicial) {
+
         energia.agregarObservador(this);
 
-        //String  fontDhurjatiPath = "file:src/main/resources/edu/fiuba/algo3/vista/fonts/Dhurjati-Regular.ttf";
-        // titulo
-        Label tituloLabel = new Label("Energia");
-
-        tituloLabel.setFont(Font.font("Helvetica", FontWeight.BOLD,12));
-        //tituloLabel.setFont(Font.loadFont(fontDhurjatiPath, 20));
-        tituloLabel.setTextFill(Color.BLUE);
+        ImageView imagenFondoEnergia = new ImageView("file:src/main/resources/edu/fiuba/algo3/vista/energia/EnergiaBoxBackGround.png");
+        imagenFondoEnergia.setFitHeight(110);
+        imagenFondoEnergia.setFitWidth(110);
+        this.getChildren().add(imagenFondoEnergia);
 
         // valor
         valorEnergiaLabel = new Label(String.valueOf(valorInicial));
-        //valorEnergiaLabel.setFont(Font.loadFont(fontDhurjatiPath, 50));
-        valorEnergiaLabel.setFont(Font.font("Helvetica", FontWeight.BOLD,24));
-        valorEnergiaLabel.setTextFill(Color.BLUE);
+        valorEnergiaLabel.setFont(Font.font("Helvetica", FontWeight.BOLD, 28));
+        valorEnergiaLabel.setTextFill(Color.SADDLEBROWN);
 
-
-        getChildren().addAll(tituloLabel, valorEnergiaLabel);
+        this.getChildren().add(valorEnergiaLabel);
 
         this.setAlignment(Pos.CENTER);
     }
@@ -40,5 +37,12 @@ public class EnergiaVista extends VBox implements ObservadorEnergia {
     @Override
     public void actualizar(int valorEnergia) {
         this.valorEnergiaLabel.setText(String.valueOf(valorEnergia));
+
+        if(valorEnergia > 0) {
+            this.valorEnergiaLabel.setTextFill(Color.SADDLEBROWN);
+        }else{
+            this.valorEnergiaLabel.setTextFill(Color.RED);
+        }
+
     }
 }
