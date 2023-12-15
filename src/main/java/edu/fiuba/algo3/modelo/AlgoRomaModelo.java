87b9aa0e9c7b15a8496911b47e889fc80b034315
Favio <fcondori@fi.uba.ar>
2023-12-15 11:52:26 -0300

    feature: agregue interfaz AlgoRomaModelo

---
+++ src/main/java/edu/fiuba/algo3/modelo/AlgoRomaModelo.java
@@ -0,0 +1,41 @@
+package edu.fiuba.algo3.modelo;
+
+import edu.fiuba.algo3.controladores.observers.ObservadorAlgoRoma;
+import edu.fiuba.algo3.modelo.gladiador.Gladiador;
+import edu.fiuba.algo3.modelo.mapa.Mapa;
+
+import java.util.ArrayList;
+
+public interface AlgoRomaModelo {
+    void agregarGladiador(String nombre)throws MaximoGladiadoresException,
+            JuegoEnCursoException, FinDelJuegoException, NombreInvalidoException;
+
+    void agregarGladiador(Gladiador gladiador)throws MaximoGladiadoresException,
+            JuegoEnCursoException, FinDelJuegoException;
+
+    void jugarTurno() throws Exception ;
+
+    int getRondasJugadas();
+
+    int cantidadDeGladiadores();
+
+    int getMaximaCantidadRondas();
+
+    ArrayList<String> getNombresGladiadoresSegunOrdenEnRonda();
+
+    ArrayList<String> getNombresGladiadoresSegunOrdenDeIngreso();
+
+    int getMinimaCantidadGladiadores();
+
+    int getMaximaCantidadGladiadores();
+
+    Mapa getMapa();
+
+    void agregarObservadorNuevoTurno(ObservadorAlgoRoma observador);
+
+    void agregarObservadorNuevoGladiador(ObservadorAlgoRoma observador);
+
+    void notificarNuevoTurno();
+
+    void notificarNuevoGladiador();
+}
