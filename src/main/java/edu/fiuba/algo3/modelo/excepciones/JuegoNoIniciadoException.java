0deb420843af9604d54e98b60190a9d22a1ba743
Favio <fcondori@fi.uba.ar>
2023-12-17 10:12:15 -0300

    elimino excepcion JuegoNoIniciadoException

--- src/main/java/edu/fiuba/algo3/modelo/excepciones/JuegoNoIniciadoException.java
+++
@@ -1,7 +0,0 @@
-package edu.fiuba.algo3.modelo.excepciones;
-
-public class JuegoNoIniciadoException extends RuntimeException{
-    public JuegoNoIniciadoException(String message) {
-        super(message);
-    }
-}
