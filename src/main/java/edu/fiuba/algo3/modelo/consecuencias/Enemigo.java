9fe1f14fef4ee9e3a51ef4a6e7e1d5e37a7b8a8c
Nazareno <nazarodriguez56@gmail.com>
2023-12-21 05:06:13 -0300

    hago que Enemigo vuelva a ser una interface en vez de una clase abstracta

--- src/main/java/edu/fiuba/algo3/modelo/consecuencias/Enemigo.java
+++ src/main/java/edu/fiuba/algo3/modelo/consecuencias/Enemigo.java
@@ -3,42 +3,42 @@ package edu.fiuba.algo3.modelo.consecuencias;
 import edu.fiuba.algo3.modelo.equipamientos.*;
 import edu.fiuba.algo3.modelo.gladiador.Energia;
 
-public abstract class Enemigo extends Consecuencia {
+public interface Enemigo {
 
     /**
      * Daño que causa al atacar contra gladiador de acuerdo al tipo de equipamiento que el mismo posee.
      * @param equipamiento equipamiento que posee el gladiador.
      * @return Energia por el valor del daño.
      */
-    public abstract Energia atacarATravesDelEquipamiento(Equipamiento equipamiento);
+    Energia atacarATravesDelEquipamiento(Equipamiento equipamiento);
 
     /**
      * Daño que causa al atacar contra gladiador que no posee equipamiento
      * @return Energia por el valor del daño.
      */
-    public abstract Energia atacarContraEquipado(SinEquipamiento sinEquipamiento);
+    Energia atacarContraEquipado(SinEquipamiento sinEquipamiento);
 
     /**
      * Daño que causa al atacar contra gladiador que posee un casco
      * @return Energia por el valor del daño.
      */
-    public abstract Energia atacarContraEquipado(Casco casco);
+    Energia atacarContraEquipado(Casco casco);
 
     /**
      * Daño que causa al atacar contra gladiador que posee una Armadura
      * @return Energia por el valor del daño.
      */
-    public abstract Energia atacarContraEquipado(Armadura armadura);
+    Energia atacarContraEquipado(Armadura armadura);
 
     /**
      * Daño que causa al atacar contra gladiador que posee un EscudoYEspada
      * @return Energia por el valor del daño.
      */
-    public abstract Energia atacarContraEquipado(EscudoYEspada escudoYEspada);
+    Energia atacarContraEquipado(EscudoYEspada escudoYEspada);
 
     /**
      * Daño que causa al atacar contra gladiador que posee una LLave
      * @return Energia por el valor del daño.
      */
-    public abstract Energia atacarContraEquipado(Llave llave);
+    Energia atacarContraEquipado(Llave llave);
 }
