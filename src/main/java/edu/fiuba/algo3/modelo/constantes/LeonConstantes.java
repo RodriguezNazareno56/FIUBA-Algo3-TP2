c372f23cffc6bc23a5a25ff0e6c9a24db6ed6733
Nazareno <nazarodriguez56@gmail.com>
2023-12-21 13:06:15 -0300

    cambio el nombre de los observables en los diagramas se secuencias

--- src/main/java/edu/fiuba/algo3/modelo/constantes/LeonConstantes.java
+++
@@ -1,9 +0,0 @@
-package edu.fiuba.algo3.modelo.constantes;
-
-public interface LeonConstantes {
-    int ATAQUE_CONTRA_EQUIPADO_SIN_EQUIPAMIENTO = 100;
-    int ATAQUE_CONTRA_EQUIPADO_CASCO = 80;
-    int ATAQUE_CONTRA_EQUIPADO_ARMADURA = 50;
-    int ATAQUE_CONTRA_EQUIPADO_ESCUDO_Y_ESPADA = 25;
-    int ATAQUE_CONTRA_EQUIPADO_LLAVE = 10;
-}
